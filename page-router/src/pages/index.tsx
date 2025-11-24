06686941692083313a1761b7a49534e90a61297b
eden <eden@rainbow.co.kr>
2025-11-24 15:11:28 +0900

    style

--- page-router/src/pages/index.tsx
+++ page-router/src/pages/index.tsx
@@ -1,7 +1,15 @@
+// CSS Module 적용
+// css 파일 이름을 클래스 이름으로 사용하는 방식
+import style from "./index.module.css";
+
 export default function Home() {
   return (
     <>
-      <h1>index page</h1>
+      {/** inline style */}
+      <h1 style={{ color: "red" }}>index page</h1>
+
+      {/** css module */}
+      <h2 className={style.h2}>test</h2>
     </>
   );
 }
