609e2a274259c7f2c24c357a487a8d93b6e300cd
eden <eden@rainbow.co.kr>
2025-11-24 17:03:12 +0900

    feat: ui

---
+++ prac-page-router/src/components/book-item.tsx
@@ -0,0 +1,19 @@
+import style from './book-item.module.css';
+import { BookData } from '@/types';
+import Link from 'next/link';
+
+export default function BookItem({ id, title, subTitle, description, author, publisher, coverImgUrl }: BookData) {
+  return (
+    <Link href={`/book/${id}`} className={style.container}>
+      <img src={coverImgUrl} alt={title} />
+      <div>
+        <div className={style.title}>{title}</div>
+        <div className={style.subTitle}>{subTitle}</div>
+        <br />
+        <div className={style.author}>
+          {author} | {publisher}
+        </div>
+      </div>
+    </Link>
+  );
+}
--- prac-page-router/src/pages/search/index.tsx
+++ prac-page-router/src/pages/search/index.tsx
@@ -1,7 +1,14 @@
+import BookItem from '@/components/book-item';
 import SearchableLayout from '../../components/searchable-layout';
-
+import books from '@/mock/book.json';
 export default function Page() {
-  return <div>Search page</div>;
+  return (
+    <div>
+      {books.map(book => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
 }
 
 Page.getLayout = (page: React.ReactNode) => {
