da02bb2c794cac86205e5846b84e4b9ffdeb420c
taewonnn <thisis_tw94@naver.com>
2025-11-25 10:00:54 +0900

    feat: ServerSideProps

--- prac-page-router/src/pages/index.tsx
+++ prac-page-router/src/pages/index.tsx
@@ -2,8 +2,29 @@ import style from './index.module.css';
 import SearchableLayout from '../components/searchable-layout';
 import books from '@/mock/book.json';
 import BookItem from '@/components/book-item';
+import { useEffect } from 'react';
+import { InferGetServerSidePropsType } from 'next';
 
-export default function Home() {
+// 이 함수는 서버에서 실행되어, 클라이언트에 전달되는 데이터를 미리 불러오는 함수!
+// 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 미리 불러오는 함수!
+export const getServerSideProps = async () => {
+  console.log('!!server side props'); // terminal log에 출력
+  const data = 'hello';
+  return {
+    // props는 무조건 객체로 반환해야 함!
+    props: { data },
+  };
+};
+
+export default function Home({ data }: { data: InferGetServerSidePropsType<typeof getServerSideProps> }) {
+  console.log('몇 번 출력?', data);
+
+  // winodw.location -> 에러나는 이유 -> 서버에서 실행되기 때문에, 서버에서 실행되면 에러가 난다.
+
+  useEffect(() => {
+    console.log(window);
+  }, []);
+
   return (
     <div className={style.container}>
       <section>
