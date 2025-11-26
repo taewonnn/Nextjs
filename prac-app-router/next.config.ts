611621ed17e78d960d5c617ab6737f7029a0c566
taewonnn <thisis_tw94@naver.com>
2025-11-26 17:25:55 +0900

    feat: data chache

--- prac-app-router/next.config.ts
+++ prac-app-router/next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  logging: {
+    fetches: {
+      fullUrl: true, // 요청 URL 전체를 로깅
+    },
+  },
   /* config options here */
   images: {
     domains: ['shopping-phinf.pstatic.net'],
--- prac-app-router/src/app/(with-searchbar)/page.tsx
+++ prac-app-router/src/app/(with-searchbar)/page.tsx
@@ -3,7 +3,7 @@ import BookItem from '../components/book-item';
 import style from './page.module.css';
 
 async function AllBokks() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/book`);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/book`, { cache: 'no-store' });
   if (!response.ok) {
     return <div>Failed to fetch books</div>;
   }
@@ -20,7 +20,7 @@ async function AllBokks() {
 }
 
 async function RecoBooks() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/book/random`);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/book/random`, { next: { revalidate: 3 } });
   if (!response.ok) {
     return <div>Failed to fetch random books</div>;
   }
