deea127dbb8e6b03dd9ffe7ef51bbcb34ec662f8
taewonnn <thisis_tw94@naver.com>
2025-11-26 14:59:53 +0900

    chore: Image 도메인 추가 설정

--- prac-app-router/next.config.ts
+++ prac-app-router/next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['shopping-phinf.pstatic.net'],
+  },
 };
 
 export default nextConfig;
