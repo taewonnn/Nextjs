0ccce07b8f7c4226691874e0a81a522cf8fdc112
taewonnn <thisis_tw94@naver.com>
2025-11-28 17:57:51 +0900

    feat: revalidatePath

--- prac-app-router/src/actions/create-review.action.ts
+++ prac-app-router/src/actions/create-review.action.ts
@@ -1,6 +1,8 @@
 // server action
 'use server';
 
+import { revalidatePath } from 'next/cache';
+
 export async function createReviewAction(formData: FormData) {
   console.log('server action called');
   console.log(formData);
@@ -25,6 +27,8 @@ export async function createReviewAction(formData: FormData) {
       return;
     }
     console.log(response.status);
+
+    revalidatePath(`/book/${bookId}`); // 성공하면 바로 방금 추가한 리뷰 보이게 하기 위해 재검증(서버 측에서만 호출 가능! - 서버컴포넌트이거나 'use server' 함수 내에서만 호출 가능)
     return;
   } catch (error) {
     console.error(error);
