c2634dc1c92df47614490a925e25dc56f28a7248
taewonnn <thisis_tw94@naver.com>
2025-12-01 10:30:22 +0900

    feat: useActionState error 처리

--- prac-app-router/src/app/components/review-editor.tsx
+++ prac-app-router/src/app/components/review-editor.tsx
@@ -2,12 +2,18 @@
 
 import { createReviewAction } from '@/actions/create-review.action';
 import style from './review-editor.module.css';
-import { useActionState } from 'react';
+import { useActionState, useEffect } from 'react';
 
 export default function ReviewEditor({ bookId }: { bookId: string }) {
   // useActionState -> server action을 클라이언트 측에서 사용할 수 있게 해주는 hook
   // const[form의 상태, form의 액션, form의 로딩 상태]= useActionState(action함수, 초기상태값)
   const [state, formAction, isPending] = useActionState(createReviewAction, null);
+
+  useEffect(() => {
+    if (state && !state.status) {
+      alert(state.error);
+    }
+  });
 
   return (
     <section>
